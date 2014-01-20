81bf25f728ea8d2af91fda0b59eac44412a7c929
hegek87 <hegelandka@vcu.edu>
2014-01-20 17:37:13 -0500

    Structure for handling the creation of a bitmap, includes at bitmap representing the first 25 million primes. A prime is given by a red pixel, while a composite is a black pixel

---
+++ bitmap.cpp
@@ -0,0 +1,127 @@
+#include "bitmap.hpp"
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <unistd.h>
+
+const static int RED = 0xFF0000;
+const static int BLACK = 0x000000;
+
+bool isPrime(int p){
+	if(p == 2){ return true; }
+	if(p < 0){ return false; }
+	for(int i = 2; i*i < p; ++i){
+		if((p%i) == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, char **argv){
+	char *fileName;
+	if(argc != 2){
+		fileName = "primes.bmp";
+	}
+	else{
+		std::cout << "Custom file name" << std::endl;
+		fileName = argv[1];
+	}
+	bmpInfoHeader iHeader;
+	iHeader.iHSize = 40;
+	iHeader.width = 500;
+	iHeader.height = 500;
+	iHeader.colorPlanes = 1;
+	iHeader.bPerPixel = 24;
+	iHeader.compression = 0;
+	
+	short int rowSize = (((iHeader.bPerPixel*iHeader.width)+31)/32)*4;
+	short int pixelArraySize = rowSize * std::abs(iHeader.height);
+	iHeader.imageSize = pixelArraySize;
+	iHeader.horizRes = 2835;
+	iHeader.vertRes = 2835;
+	iHeader.colorPallette = 0;
+	iHeader.importantColors = 0;
+	
+	bmpHeader header;
+	header.magic1 = 'B';
+	header.magic2 = 'M';
+	header.size = 54 + pixelArraySize;
+	header.reserved1 = 0;
+	header.reserved2 = 0;
+	header.offset = 54;
+	std::cout << header.size << std::endl;
+	std::cout << sizeof(char) << std::endl;
+	std::cout << sizeof(short int) << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	std::cout << sizeof(long) << std::endl;
+	
+	//write bmpHeader
+	std::ofstream writeFile;
+	writeFile.open(fileName);
+	writeFile << header.magic1;				// 1 byte
+	writeFile << header.magic2;				// 1 byte
+	writeFile.write((char *)&header.size, 4);		// 4 bytes
+	writeFile.write((char *)&header.reserved1, 2);	// 2 bytes
+	writeFile.write((char *)&header.reserved2, 2);	// 2 bytes
+	writeFile.write((char *)&header.offset, 4);	// 4 bytes
+									// 14 bytes written
+	
+	//write bmpInfoHeader
+	writeFile.write((char *)&iHeader.iHSize, 4);		// 4 bytes
+	writeFile.write((char *)&iHeader.width, 4);		// 4 bytes
+	writeFile.write((char *)&iHeader.height, 4);		// 4 bytes
+	writeFile.write((char *)&iHeader.colorPlanes, 2);	// 2 bytes
+	writeFile.write((char *)&iHeader.bPerPixel, 2);		// 2 bytes
+	writeFile.write((char *)&iHeader.compression, 4);	// 4 bytes
+	writeFile.write((char *)&iHeader.imageSize, 4);		// 4 bytes
+	writeFile.write((char *)&iHeader.horizRes, 4);		// 4 bytes
+	writeFile.write((char *)&iHeader.vertRes, 4);		// 4 bytes
+	writeFile.write((char *)&iHeader.colorPallette, 4);	// 4 bytes
+	writeFile.write((char *)&iHeader.importantColors, 4);	// 4 bytes
+									// 40 bytes written
+	/*
+	writeFile << iHeader.iHSize;			// 4 bytes
+	writeFile << iHeader.width;			// 4 bytes
+	writeFile << iHeader.height;			// 4 bytes
+	writeFile << iHeader.colorPlanes;		// 2 bytes
+	writeFile << iHeader.bPerPixel;		// 2 bytes
+	writeFile << iHeader.compression;		// 4 bytes
+	writeFile << iHeader.imageSize;		// 4 bytes
+	writeFile << iHeader.horizRes;		// 4 bytes
+	writeFile << iHeader.vertRes;			// 4 bytes
+	writeFile << iHeader.colorPallette;		// 4 bytes
+	writeFile << iHeader.importantColors;	// 4 bytes
+		*/						// 40 bytes written	
+	//12 bytes written
+	
+	for(int i = iHeader.width-1; i >= 0; --i){
+		for(int j = iHeader.height-1; j >= 0; --j){
+			if(isPrime(i*iHeader.width+j)){
+				writeFile.write((char *)&RED, 3);
+			}
+			else{
+				writeFile.write((char *)&BLACK, 3);
+			}
+		}
+	}
+	
+	/*
+	for(int i = 0; i < iHeader.width; ++i){
+		for(int j = 0; i < iHeader.height; ++j){
+			bool prime = isPrime(i*iHeader.width+j);
+			if(prime){
+				writeFile << RED;
+			}
+			else{
+				writeFile << BLACK;
+			}
+		}
+	}*/
+	
+	writeFile.close();
+	
+	std::cout << isPrime(10) << std::endl;
+	std::cout << isPrime(11) << std::endl;
+	return 0;
+}
---
+++ bitmap.hpp
@@ -0,0 +1,37 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+//#define RED		0xFF0000
+//#define BLACK	0X000000
+/*
+* char is 1 byte
+* short int is 2 bytes
+* long is 4 bytes
+*/
+// 14 bytes
+struct bmpHeader{
+	char magic1;		// 'B' - magic number
+	char magic2;		// 'M' - magic number
+	long size;			// file size
+	short int reserved1;	// Just reserved space, because of the
+	short int reserved2;	// reserved, size can be 8 bytes
+	int offset;		// 14+40 - distance from start to image data
+};
+
+// 40 bytes
+struct bmpInfoHeader{
+	long iHSize;		// 40
+	long width;			// image width
+	long height;		// image height
+	short int colorPlanes;	// 1
+	short int bPerPixel; 	// 24 - number of bits per pixel
+	long compression; 	// 0 - not used
+	long imageSize;		// image data, not file size
+	long horizRes;		// horizontal resolution - pixels per meter
+	long vertRes;		// vertical resolution - pixels per meter
+	long colorPallette;	// 0 (2^n) - number of colors available
+	long importantColors;	// 0 - all colors are important
+};
+
+bool isPrime(int);
+#endif
